87d45f7774bfff531ae8fa1ee6ebf7b307e057e1
Daniel Hartwich <daniel.hartwich@xing.com>
2018-06-22 17:50:17 +0200

    AS 3.2 beta1 support

--- app/src/main/java/com/jdapplications/mvitest/main/ui/main/MainViewState.kt
+++ app/src/main/java/com/jdapplications/mvitest/main/ui/main/MainViewState.kt
@@ -3,8 +3,7 @@ package com.jdapplications.mvitest.main.ui.main
 import com.jdapplications.mvi_core.mvi.MviViewState
 
 data class MainViewState(val username: String, val loading: Boolean, val feedbackSubmitted: Boolean, val error: Boolean,
-                         val userList: List<String>) :
-        MviViewState {
+                         val userList: List<String>) : MviViewState {
     companion object {
         fun initial(): MainViewState {
             return MainViewState("", false, false, false, emptyList())
